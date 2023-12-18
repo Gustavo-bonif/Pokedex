6ce167b83d908f7d00677cb6ca9b51912b01c281
Gustavo-bonif <122123308+Gustavo-bonif@users.noreply.github.com>
2023-12-18 19:48:36 -0300

    passando pokemom par outra página

--- js/individuo.js
+++ js/individuo.js
@@ -1,22 +1,27 @@
 
 const corpo = document.getElementById('container')
-const id = sessionStorage.getItem('individuo');
+const numero = sessionStorage.getItem('individuo');
+const id  = numero-1;
 
 
-
-
-function convertePokemonIndividual(pokemon){
-    return`<h1> BOM Dia Mundo!</h1>`
+function convertePokemonIndividual(pokemon) {
+    // return `<h1> BOM Dia Mundo!</h1> ${pokemon.name}`
 }
 
 
-     pokeApi.getPokemons(5,1)
-     .then((pokemons = []) => {
-        const newHTML = pokemons.map(convertePokemonIndividual).join('')
-         corpo.innerHTML = newHTML
-     })
+// (function(){
 
-     corpo.innerHTML = convertePokemonIndividual();
+//     pokeApi.getPokemons(id, 1)
+//     .then((pokemons = []) => {
+//         const newHTML = pokemons.map(convertePokemonIndividual).join('')
+//         corpo.innerHTML = newHTML
+//     })
+
+// })()
+
+
+
+//corpo.innerHTML = convertePokemonIndividual();
 
 
 
